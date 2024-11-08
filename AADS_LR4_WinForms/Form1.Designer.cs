0c813a1362691c5ba0a19f6bb87b345c6b6ea1ac
Anastasia Lyah <lyahanastasya@gmail.com>
2024-11-08 18:03:23 +0200

    Добавьте файлы проекта.

---
+++ AADS_LR4_WinForms/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace AADS_LR4_WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCheckDuplicates = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnPostOrderTraversal = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.SystemColors.Window;
+            this.txtInput.Location = new System.Drawing.Point(225, 16);
+            this.txtInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(192, 29);
+            this.txtInput.TabIndex = 0;
+            // 
+            // treeView
+            // 
+            this.treeView.BackColor = System.Drawing.SystemColors.Window;
+            this.treeView.Location = new System.Drawing.Point(6, 28);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(388, 309);
+            this.treeView.TabIndex = 1;
+            this.treeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.btnAdd.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnAdd.Location = new System.Drawing.Point(17, 60);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(192, 40);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Додати вузол";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearch.FlatAppearance.BorderSize = 2;
+            this.btnSearch.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnSearch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSearch.Location = new System.Drawing.Point(17, 106);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(194, 40);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Знайти вузол";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelete.FlatAppearance.BorderSize = 2;
+            this.btnDelete.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnDelete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnDelete.Location = new System.Drawing.Point(225, 60);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(192, 40);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Видалити вузол";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCheckDuplicates
+            // 
+            this.btnCheckDuplicates.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnCheckDuplicates.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCheckDuplicates.FlatAppearance.BorderSize = 2;
+            this.btnCheckDuplicates.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnCheckDuplicates.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnCheckDuplicates.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnCheckDuplicates.Location = new System.Drawing.Point(225, 106);
+            this.btnCheckDuplicates.Name = "btnCheckDuplicates";
+            this.btnCheckDuplicates.Size = new System.Drawing.Size(192, 40);
+            this.btnCheckDuplicates.TabIndex = 5;
+            this.btnCheckDuplicates.Text = "Знайти дублікати";
+            this.btnCheckDuplicates.UseVisualStyleBackColor = false;
+            this.btnCheckDuplicates.Click += new System.EventHandler(this.btnCheckDuplicates_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.BackColor = System.Drawing.SystemColors.Window;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 27;
+            this.listBox.Location = new System.Drawing.Point(6, 28);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(208, 355);
+            this.listBox.TabIndex = 6;
+            this.listBox.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 27);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Введіть вузол дерева:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(423, 106);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 391);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Зворотній обхід дерева";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Lavender;
+            this.groupBox2.Controls.Add(this.treeView);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox2.Location = new System.Drawing.Point(17, 152);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox2.Size = new System.Drawing.Size(400, 345);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Бінарне дерево";
+            // 
+            // btnPostOrderTraversal
+            // 
+            this.btnPostOrderTraversal.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnPostOrderTraversal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPostOrderTraversal.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Silver;
+            this.btnPostOrderTraversal.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnPostOrderTraversal.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnPostOrderTraversal.Location = new System.Drawing.Point(423, 60);
+            this.btnPostOrderTraversal.Name = "btnPostOrderTraversal";
+            this.btnPostOrderTraversal.Size = new System.Drawing.Size(220, 40);
+            this.btnPostOrderTraversal.TabIndex = 11;
+            this.btnPostOrderTraversal.Text = "Показати обхід";
+            this.btnPostOrderTraversal.UseVisualStyleBackColor = false;
+            this.btnPostOrderTraversal.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(654, 501);
+            this.Controls.Add(this.btnPostOrderTraversal);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCheckDuplicates);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtInput);
+            this.Font = new System.Drawing.Font("Onest", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Бінарне дерево";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCheckDuplicates;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnPostOrderTraversal;
+    }
+}
+
---
+++ AADS_LR4_WinForms/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AADS_LR4_WinForms{
+    
+    public partial class Form1 : Form
+    {
+        private BinaryTree binaryTree;
+        public Form1()
+        {
+            InitializeComponent();
+            binaryTree = new BinaryTree();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {}
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            AddNodeAndUpdateTreeView();
+        }
+        private void AddNodeAndUpdateTreeView()
+        {
+            if (int.TryParse(txtInput.Text, out int value))
+            {
+                bool wasDuplicate = binaryTree.ContainsDuplicate(binaryTree.Root, value);
+                binaryTree.Insert(value);
+                UpdateTreeView();
+
+                if (!wasDuplicate && binaryTree.ContainsDuplicate(binaryTree.Root, value))
+                {
+                    MessageBox.Show($"Значення {value} зустрічається двічі в дереві.");
+                }
+                txtInput.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Будь ласка, введіть коректне ціле число.");
+            }
+        }
+        private void UpdateTreeView()
+        {
+            treeView.Nodes.Clear();
+            if (binaryTree.Root != null)
+            {
+                AddNodeToTreeView(binaryTree.Root, null);
+            }
+            treeView.ExpandAll();
+        }
+        private void AddNodeToTreeView(BinaryTreeNode node, TreeNode parent)
+        {
+            TreeNode newNode = new TreeNode(node.Value.ToString());
+
+            if (parent == null)
+            {
+                treeView.Nodes.Add(newNode);
+            }
+            else
+            {
+                parent.Nodes.Add(newNode);
+            }
+
+            if (node.Left != null)
+            {
+                AddNodeToTreeView(node.Left, newNode);
+            }
+            if (node.Right != null)
+            {
+                AddNodeToTreeView(node.Right, newNode);
+            }
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (TryGetInputValue(out int value))
+            {
+                bool found = binaryTree.Search(binaryTree.Root, value);
+                MessageBox.Show(found ? "Значення знайдено." : "Значення не знайдено.");
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (TryGetInputValue(out int value))
+            {
+                binaryTree.Root = binaryTree.Delete(binaryTree.Root, value);
+                UpdateTreeView();
+            }
+        }
+        private void btnCheckDuplicates_Click(object sender, EventArgs e)
+        {
+            if (TryGetInputValue(out int value))
+            {
+                bool hasDuplicate = binaryTree.ContainsDuplicate(binaryTree.Root, value);
+                MessageBox.Show(hasDuplicate ? "Значення зустрічається двічі." : "Значення не зустрічається двічі.");
+            }
+        }
+        private bool TryGetInputValue(out int value)
+        {
+            bool success = int.TryParse(txtInput.Text, out value);
+            if (!success)
+            {
+                MessageBox.Show("Будь ласка, введіть коректне ціле число.");
+                txtInput.Clear();
+            }
+            return success;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            PostOrderTraversal(binaryTree.Root);
+        }
+        private void PostOrderTraversal(BinaryTreeNode node)
+        {
+            if (node != null)
+            {
+                PostOrderTraversal(node.Left);
+                PostOrderTraversal(node.Right);
+                listBox.Items.Add(node.Value);
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+    }
+    public class BinaryTreeNode
+    {
+        public int Value { get; set; }
+        public BinaryTreeNode Left { get; set; }
+        public BinaryTreeNode Right { get; set; }
+
+        public BinaryTreeNode(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+    public class BinaryTree
+    {
+        public BinaryTreeNode Root { get; set; }
+
+        public void Insert(int value)
+        {
+            Root = Insert(Root, value);
+        }
+
+        private BinaryTreeNode Insert(BinaryTreeNode node, int value)
+        {
+            if (node == null)
+                return new BinaryTreeNode(value);
+
+            if (value <= node.Value)
+                node.Left = Insert(node.Left, value);
+            else
+                node.Right = Insert(node.Right, value);
+
+            return node;
+        }
+        public bool ContainsDuplicate(BinaryTreeNode node, int targetValue)
+        {
+            return CountOccurrences(node, targetValue) > 1;
+        }
+        private int CountOccurrences(BinaryTreeNode node, int targetValue)
+        {
+            if (node == null)
+                return 0;
+
+            int count = node.Value == targetValue ? 1 : 0;
+            return count + CountOccurrences(node.Left, targetValue) + CountOccurrences(node.Right, targetValue);
+        }
+        public bool Search(BinaryTreeNode node, int value)
+        {
+            if (node == null) return false;
+            if (node.Value == value) return true;
+
+            return value < node.Value ? Search(node.Left, value) : Search(node.Right, value);
+        }
+        public BinaryTreeNode Delete(BinaryTreeNode node, int value)
+        {
+            if (node == null) return null;
+
+            if (value < node.Value)
+                node.Left = Delete(node.Left, value);
+            else if (value > node.Value)
+                node.Right = Delete(node.Right, value);
+            else
+            {
+                if (node.Left == null) return node.Right;
+                if (node.Right == null) return node.Left;
+
+                BinaryTreeNode minNode = GetMinValueNode(node.Right);
+                node.Value = minNode.Value;
+                node.Right = Delete(node.Right, minNode.Value);
+            }
+            return node;
+        }
+        private BinaryTreeNode GetMinValueNode(BinaryTreeNode node)
+        {
+            while (node.Left != null)
+                node = node.Left;
+            return node;
+        }
+    }
+}
